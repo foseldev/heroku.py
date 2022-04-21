10587457369d02d64841f3a236f5364dfb16d06f
foseldev <104160611+foseldev@users.noreply.github.com>
2022-04-21 19:54:10 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,8 @@
 #from selenium import webdriver
 
-from selenium.webdriver.common.by import By
-from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.firefox.service import Service
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
-from webdriver_manager.firefox import GeckoDriverManager
 
-URL = ""
+
+URL = "https://www.google.com/"
 TIMEOUT = 20
 
 st.title("Test Selenium")
