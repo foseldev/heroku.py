b1e4e0e3de4b853224aad8681fc6b1959e4f4633
foseldev <104160611+foseldev@users.noreply.github.com>
2022-04-21 19:33:43 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
 import streamlit as st
 
-st.title("ff")
+#st.title("ff")
 
 print('ñ')
+
+
+from selenium import webdriver
+import time
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-gpu")
+driver = webdriver.Chrome(options=chrome_options)
+
+driver.get("https://www.google.com")
+print('El texto es:')
+time.sleep(1)
+driver.find_element_by_xpath("//div[contains(text(),'Acepto')]").click()
+print('si')
+st.title("ff")
+driver.quit()
