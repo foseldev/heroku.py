5618e7ab2a630ec510e03536ac4620af22a6aa33
foseldev <104160611+foseldev@users.noreply.github.com>
2022-04-21 19:20:32 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,1 @@
-from selenium import webdriver
-import time
-
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument("--no-sandbox")
-#chrome_options.add_argument("--headless")
-chrome_options.add_argument("--disable-gpu")
-driver = webdriver.Chrome(options=chrome_options)
-
-driver.get("https://www.google.com")
-print('El texto es:')
-time.sleep(1)
-driver.find_element_by_xpath("//div[contains(text(),'Acepto')]").click()
 print('si')
-
-driver.quit()
